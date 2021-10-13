71dd59777d2a5fd48a51e53fe7f7fbecff577384
Alexei Raiu <alex.rayu@gmail.com>
2021-10-13 10:45:48 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mobile Website Template
+
+The compiled files are placed in "/www"
+
+- from `src/scss` to `www/index.css`
+- from `src/js` to `www/bundle.js`
+- from `src/views` to `www/`
+- from `src/images` to `www/images`
+
+## Getting Started
+
+```
+nvm use 12
+```
+
+1. Installation
+```
+npm install
+```
+
+2. Build
+```
+npm run build
+```
+
+Based on https://github.com/Vincenius/static-website-template by Vincent Will.
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, './www'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    ]
+  }
+}

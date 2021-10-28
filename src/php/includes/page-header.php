d12bd800cd89c24f64262d44339acb65b1a60855
Alexei Raiu <alex.rayu@gmail.com>
2021-10-28 10:13:32 +0300

    Port fixes.

--- src/php/includes/page-header.php
+++ src/php/includes/page-header.php
@@ -1,6 +1,6 @@
 <header class="page-header">
   <div class="container">
-    <div class="logo">LOGO</div>
+    <div class="logo"><a href="/" title="Company Home">LOGO</a></div>
     <div class="slogan">Slogan - as much as it fits here.</div>
     <div class="menu-toggle">Toggle Menu</div>
   </div>
